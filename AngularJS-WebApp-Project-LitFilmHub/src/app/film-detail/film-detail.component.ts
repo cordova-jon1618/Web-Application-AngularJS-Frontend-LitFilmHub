de2f83c832b9395d6bbfd213b24d53beef30994d
Jonathan <cordovaj63@gmail.com>
2023-11-16 15:26:58 -0800

    Generated component template for home, nav-menu, search, book-detail, film-detail

---
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/film-detail/film-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-film-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './film-detail.component.html',
+  styleUrl: './film-detail.component.css'
+})
+export class FilmDetailComponent {
+
+}
