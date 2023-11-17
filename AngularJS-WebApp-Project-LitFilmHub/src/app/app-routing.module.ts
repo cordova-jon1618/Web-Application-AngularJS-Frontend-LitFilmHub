71cdc9f2f29359c206c07b305a8d78fa9b1c7d52
Jonathan <cordovaj63@gmail.com>
2023-11-17 09:13:40 -0800

    Added navigation bar for pages by updating app.component.html, nav-menu.component.html, app-routing.module.ts

--- AngularJS-WebApp-Project-LitFilmHub/src/app/app-routing.module.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { FilmDetailComponent } from './film-detail/film-detail.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{ path: '', component: HomeComponent, pathMatch: 'full' },
+{ path: 'search', component: SearchComponent },
+{ path: 'book-detail', component: BookDetailComponent },
+{ path: 'film-detail', component: FilmDetailComponent },
+];
+17
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
