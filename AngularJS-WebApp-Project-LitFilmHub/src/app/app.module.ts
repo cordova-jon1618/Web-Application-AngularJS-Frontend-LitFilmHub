614396a861e8a539f379c49c1b794d906b1ff019
Jonathan <cordovaj63@gmail.com>
2023-11-17 08:56:56 -0800

    Added Home, Nav-Menu, Book-Detail, Film-Detail, Search Components

--- AngularJS-WebApp-Project-LitFilmHub/src/app/app.module.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { FilmDetailComponent } from './film-detail/film-detail.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    BookDetailComponent,
+    FilmDetailComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
