d29444329a3d92ac55834cc21a62c1c269698840
Jonathan <cordovaj63@gmail.com>
2023-12-09 14:04:33 -0800

    Updated misc information

--- AngularJS-WebApp-Project-LitFilmHub/src/app/login/login.component.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/login/login.component.ts
@@ -1,25 +1,3 @@
-// import { Component } from '@angular/core';
-// import { FormGroup, FormBuilder } from '@angular/forms';
-
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css']
-// })
-// export class LoginComponent {
-//   form: FormGroup;
-
-//   constructor(private formBuilder: FormBuilder) {
-//     this.form = this.formBuilder.group({
-//       
-//     });
-//   }
-
-//   onSubmit() {
-//     
-//   }
-
-// }
 
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormGroup, FormGroup, FormControl, Validators } from '@angular/forms';
@@ -32,7 +10,7 @@ import { LoginRequest } from './login-request';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
-  form!: UntypedFormGroup;
+   form!: UntypedFormGroup;
 
   constructor(protected authService: AuthService) {}
 
--- AngularJS-WebApp-Project-LitFilmHub/src/environments/environment.development.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    baseUrl: 'http://localhost:7182'
-    };
+    baseUrl: 'https://localhost:7157'
+};  
