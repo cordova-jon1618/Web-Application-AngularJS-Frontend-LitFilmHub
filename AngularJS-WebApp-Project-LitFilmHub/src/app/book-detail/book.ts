501c0f6eae981c5dff9dc368e3e7c4cb9315c6d5
Jonathan <cordovaj63@gmail.com>
2023-12-09 08:46:58 -0800

    book.ts and film.ts and environments typescripts files created

---
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/book-detail/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    id: number;
+    title: string | null;
+    author: string | null;
+    genre: string | null;
+    publicationYear: number | null;
+    isbn: string | null;
+    summary: string | null;
+    coverImageUrl: string | null;
+  }
+  
---
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/film-detail/film.ts
@@ -0,0 +1,11 @@
+export interface Film {
+    id: number;
+    title: string | null;
+    director: string | null;
+    genre: string | null;
+    releaseYear: number | null;
+    rating: string | null;
+    synopsis: string | null;
+    posterImageUrl: string | null;
+  }
+  
