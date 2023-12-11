a0953b982c7b892fa50d26ae1539a207360ac732
Jonathan <cordovaj63@gmail.com>
2023-12-11 11:46:09 -0800

    Fixed routing issue with book-details component.

--- AngularJS-WebApp-Project-LitFilmHub/src/app/book-detail/book-detail.component.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/book-detail/book-detail.component.ts
@@ -9,12 +9,13 @@ import { environment } from 'src/environments/environment.development';
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.css']
 })
+
 export class BookDetailComponent {
   books: Book[] = [];
   currentIndex = 0;
 
   constructor(private http: HttpClient) {
-    this.http.get<Book[]>(environment.baseUrl + '/api/book-detail').subscribe({
+    this.http.get<Book[]>(environment.baseUrl + '/api/book/book-detail').subscribe({
       next: (result) => {
         this.books = result;
         this.currentIndex = 0;
