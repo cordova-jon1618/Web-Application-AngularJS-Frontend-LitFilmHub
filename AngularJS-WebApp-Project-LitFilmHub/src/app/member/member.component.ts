535349d10cd8ea3aec3086c7dc806180413ac2e4
Jonathan <cordovaj63@gmail.com>
2023-12-11 18:28:30 -0800

    Adding member component functionality

--- AngularJS-WebApp-Project-LitFilmHub/src/app/member/member.component.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/member/member.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Member } from './member';
 
 @Component({
   selector: 'app-member',
   templateUrl: './member.component.html',
   styleUrls: ['./member.component.css']
 })
-export class MemberComponent {
 
-}
+export class MemberComponent {
+    members: Member[] = [];
+    currentIndex = 0;
+  
+    constructor(private http: HttpClient) {
+      this.http.get<Member[]>(environment.baseUrl + '/api/member').subscribe({
+        next: (result) => {
+          this.members = result;
+          this.currentIndex = 0;
+        },
+        error: (error) => {
+          console.error('Error fetching members:', error);
+        }
+      });
+    }//end constructor
+  
+    onNextClick() {
+      if (this.members.length > 0) {
+        this.currentIndex = (this.currentIndex + 1) % this.members.length;
+      }
+    }
+  
+    get currentMember() {
+      return this.members.length > 0 ? this.members[this.currentIndex] : null;
+    }
+  }
+  
+  
---
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/member/member.ts
@@ -0,0 +1,11 @@
+export interface Member {
+    memberID: number;
+    firstName: string | null;
+    middleInitial: string | null;
+    lastName: string | null;
+    city: string | null;
+    country: string | null;
+    email: string | null;
+    username: string | null;
+    password: string | null;
+}
