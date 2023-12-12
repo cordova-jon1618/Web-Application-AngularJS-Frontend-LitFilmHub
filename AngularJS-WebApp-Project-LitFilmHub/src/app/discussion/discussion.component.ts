36391340288deacb235105c9bfbabf9079e41608
Jonathan <cordovaj63@gmail.com>
2023-12-12 15:53:37 -0800

    Added Discussion component functionality for login, styling and data entry

--- AngularJS-WebApp-Project-LitFilmHub/src/app/discussion/discussion.component.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/discussion/discussion.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Discussion } from './discussion';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
+import { AuthService } from '../login/auth.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-discussion',
@@ -11,9 +13,37 @@ import { environment } from 'src/environments/environment.development';
 
 export class DiscussionComponent {
   discussions: Discussion[] = [];
-  currentIndex = 0;
+  currentIndex = 0; 
+  isLoggedIn = false;
+  message: string = '';
+  private destroySubject = new Subject();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private authService: AuthService) {
+    // Subscribe to the auth status
+    this.authService.authStatus.pipe(takeUntil(this.destroySubject))
+    .subscribe(result => {
+      this.isLoggedIn = result;
+
+      // Fetch discussions only when the user is logged in
+      if (this.isLoggedIn) {
+        this.fetchDiscussions();
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenicated(); // Ensure the method name matches your AuthService
+    if (this.isLoggedIn) {
+      this.fetchDiscussions();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubject.next(true);
+    this.destroySubject.complete();
+  }
+
+  fetchDiscussions() {
     this.http.get<Discussion[]>(environment.baseUrl + '/api/discussion').subscribe({
       next: (result) => {
         this.discussions = result;
@@ -23,18 +53,52 @@ export class DiscussionComponent {
         console.error('Error fetching discussions:', error);
       }
     });
-  }//end constructor
-
-  onNextClick() {
-    console.log("Discussion length: ", this.discussions.length);
-    if (this.discussions.length > 0) {
-      this.currentIndex = (this.currentIndex + 1) % this.discussions.length;
-      console.log("Discussion index: ", this.currentIndex);
-    }
   }
+
+  // sendMessage(message: string) {
+  //   console.log('Message to send:', message);
+  //   // Add logic to handle sending the message
+  //   // Reset the message input after sending
+  //   this.message = '';
+  // }
+
+  sendMessage(message: string) {
+    // Check if the message is empty before sending
+    if (message.trim() === '') {
+      console.log('Message is empty. Not sending.');
+      return;
+    }
+  
+    console.log('Message to send:', message);
+  
+    // Send the message to the backend
+    this.http.post(environment.baseUrl + '/api/discussion/addMessage', JSON.stringify(message), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).subscribe({
+      next: () => {
+        console.log('Message sent successfully');
+        // Optionally, you can fetch discussions again to refresh the list
+        this.fetchDiscussions();
+      },
+      error: (error) => {
+        console.error('Error sending message:', error);
+      }
+    });
+  }
+  
+  // onNextClick() {
+  //   console.log("Discussion length: ", this.discussions.length);
+  //   if (this.discussions.length > 0) {
+  //     this.currentIndex = (this.currentIndex + 1) % this.discussions.length;
+  //     console.log("Discussion index: ", this.currentIndex);
+  //   }
+  // }
 
   get currentDiscussion() {
     return this.discussions.length > 0 ? this.discussions[this.currentIndex] : null;
   }
+
 }
 
