ce6b3022f1bb072f0308e66fdd126cf8f2e42b46
Jonathan <cordovaj63@gmail.com>
2023-12-12 09:36:27 -0800

    Added login functionality to allow access to member profile

--- AngularJS-WebApp-Project-LitFilmHub/src/app/member/member.component.ts
+++ AngularJS-WebApp-Project-LitFilmHub/src/app/member/member.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { Member } from './member';
+
+import { AuthService } from '../login/auth.service';
+import  { Subject, takeUntil } from 'rxjs';
+
 
 @Component({
   selector: 'app-member',
@@ -9,11 +13,39 @@ import { Member } from './member';
   styleUrls: ['./member.component.css']
 })
 
-export class MemberComponent {
-    members: Member[] = [];
-    currentIndex = 0;
-  
-    constructor(private http: HttpClient) {
+export class MemberComponent implements OnInit, OnDestroy {
+  members: Member[] = [];
+  currentIndex = 0;
+  isLoggedIn: boolean = false;
+  private destroySubject = new Subject();
+
+  constructor(private http: HttpClient, private authService: AuthService) {
+    // Subscribe to the auth status
+    this.authService.authStatus.pipe(takeUntil(this.destroySubject))
+    .subscribe(result => {
+      this.isLoggedIn = result;
+
+      // Fetch members only when the user is logged in
+      if (this.isLoggedIn) {
+        this.fetchMembers();
+        console.log("Member should be fetched.")
+      }
+    });
+  }//end constructor
+
+    ngOnInit(): void {
+      this.isLoggedIn = this.authService.isAuthenicated();
+      if (this.isLoggedIn) {
+        this.fetchMembers();
+      }
+    }
+
+    ngOnDestroy(): void {
+      this.destroySubject.next(true);
+      this.destroySubject.complete();
+    }
+
+    fetchMembers() {
       this.http.get<Member[]>(environment.baseUrl + '/api/member').subscribe({
         next: (result) => {
           this.members = result;
@@ -23,8 +55,8 @@ export class MemberComponent {
           console.error('Error fetching members:', error);
         }
       });
-    }//end constructor
-  
+    }
+    
     onNextClick() {
       if (this.members.length > 0) {
         this.currentIndex = (this.currentIndex + 1) % this.members.length;
